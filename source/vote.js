c87428695166ec6860f8bfbb57570afb542c921d
biruk <birukanley2022@gmail.com>
2022-03-17 21:36:45 +0300

    age restirction

--- source/vote.js
+++ source/vote.js
@@ -1,5 +1,16 @@
- function sample(){
-    document.getElementById("texte").innerHTML = "sample try works"
- }
-
  
+ function vote(){
+   let age=document.getElementById("vote").value
+   if (isNaN(age)){
+       votetext="texts are not allowed enter number please"
+   }
+   else if(age<18){
+       votetext=" sorry you age is not enough to vote"
+   }
+   else{
+       votetext="you  vote has been successfully completed"
+   }
+
+   document.getElementById("paragrapgh").innerHTML=votetext
+}
+
