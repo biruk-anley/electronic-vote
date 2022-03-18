5885ef2193dbf2f78b64f78ec79e7ee728fa0e35
biruk <birukanley2022@gmail.com>
2022-03-18 11:57:15 +0300

    add functionality over age

--- source/vote.js
+++ source/vote.js
@@ -1,13 +1,22 @@
  
  function vote(){
    let age=document.getElementById("vote").value
+   const val = document.getElementById('vote').style.borderColor = "rgb(247, 81, 81)"
+   let votetext;
    if (isNaN(age)){
        votetext="texts are not allowed enter number please"
+       document.getElementsByTagName(input)
    }
-   else if(age<18){
-       votetext=" sorry you age is not enough to vote"
-   }
-   else{
+
+
+   else if(parseInt(age)<18){
+   val
+   votetext=" sorry you age is not enough to vote" + val
+    }
+      else if(age = " "){
+        votetext = "you should enter your age";
+    }
+   else {
        votetext="you  vote has been successfully completed"
    }
 
