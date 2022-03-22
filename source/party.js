a2f520d89effc999afd1d9a8f12e2ddae9a5add1
biruk <birukanley2022@gmail.com>
2022-03-22 14:13:11 +0300

    check validity for party name

--- source/party.js
+++ source/party.js
@@ -1,6 +1,24 @@
-document.getElementById("partysubmit").addEventListener('click' ,(event) => {
+function select() {
+    let partyholder;
+    let partyName = document.querySelector("input[name='fav_language']:checked");
+    console.log(partyName)
+    if (partyName ==null) {
+        partyholder = "please enter pary name";
+        document.getElementById("errormessage").innerHTML = partyholder;
+        
+    }
+    else if(partyName!=null){
+        return window.location.href = "successfull.html";
+    }
+
+}
+
+
+
+document.getElementById("partysubmit").addEventListener('click', (event) => {
     event.preventDefault();
-    
+    select();
+
 })
     
-    ;
+    
